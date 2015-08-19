5297d525423c9e5898458ba29523a00c3e37a417
devprovers <dev_provers@abv.bg>
2015-08-19 21:45:42 +0300

    Automation Test Update

--- Automation_Tests/src/test/java/LoginTest.java
+++ Automation_Tests/src/test/java/LoginTest.java
@@ -1,4 +1,7 @@
-import org.junit.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 import org.openqa.selenium.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
